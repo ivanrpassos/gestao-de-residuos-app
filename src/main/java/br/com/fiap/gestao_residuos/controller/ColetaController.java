89a0e83569e80b4c8cb690466f50d8af28e9c9da
Ivan Passos <ivan.passos@sinkle.co>
2025-11-05 03:53:32 -0300

    feat(coleta): Added MVC to Coleta

---
+++ src/main/java/br/com/fiap/gestao_residuos/controller/ColetaController.java
@@ -0,0 +1,47 @@
+package br.com.fiap.gestao_residuos.controller;
+
+import br.com.fiap.gestao_residuos.model.Coleta;
+import br.com.fiap.gestao_residuos.service.ColetaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class ColetaController {
+
+    @Autowired
+    private ColetaService coletaService;
+
+    @GetMapping("/coletas")
+    public ResponseEntity<List<Coleta>> listarTodos() {
+        return ResponseEntity.ok(coletaService.listarTodos());
+    }
+
+    @GetMapping("/coleta/{id}")
+    public ResponseEntity<Coleta> buscarPorId(@PathVariable Long id) {
+        return coletaService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/coleta")
+    public ResponseEntity<Coleta> salvar(@RequestBody Coleta coleta) {
+        Coleta novo = coletaService.salvar(coleta);
+        return ResponseEntity.ok(novo);
+    }
+
+    @PutMapping("/coleta/{id}")
+    public ResponseEntity<Coleta> atualizar(@PathVariable Long id, @RequestBody Coleta coleta) {
+        Coleta atualizado = coletaService.atualizar(id, coleta);
+        return ResponseEntity.ok(atualizado);
+    }
+
+    @DeleteMapping("/coleta/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        coletaService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/br/com/fiap/gestao_residuos/service/ColetaService.java
@@ -0,0 +1,46 @@
+package br.com.fiap.gestao_residuos.service;
+
+import br.com.fiap.gestao_residuos.model.Coleta;
+import br.com.fiap.gestao_residuos.repository.ColetaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ColetaService {
+
+    @Autowired
+    private ColetaRepository coletaRepository;
+
+
+    public List<Coleta> listarTodos() {
+        return coletaRepository.findAll();
+    }
+
+    public Optional<Coleta> buscarPorId(Long id) {
+        return coletaRepository.findById(id);
+    }
+
+    public Coleta salvar(Coleta contenedor) {
+        return coletaRepository.save(contenedor);
+    }
+
+    public Coleta atualizar(Long id, Coleta coletaParam) {
+        return coletaRepository.findById(id)
+                .map(coleta -> {
+                    coleta.setRota(coletaParam.getRota());
+                    coleta.setStatus(coletaParam.getStatus());
+                    coleta.setContenedor(coletaParam.getContenedor());
+                    coleta.setDataAgendada(coletaParam.getDataAgendada());
+
+                    return coletaRepository.save(coleta);
+                })
+                .orElseThrow(() -> new RuntimeException("Coleta não encontrada com ID: " + id));
+    }
+
+    public void excluir(Long id) {
+        coletaRepository.deleteById(id);
+    }
+}
